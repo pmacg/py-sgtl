295169acd2c3bb678c7f72e291226bcf9102971f
Peter Macgregor <macgregor.pr@gmail.com>
2021-11-10 21:18:26 +0000

    Bump version

--- docs/source/changelog.rst
+++ docs/source/changelog.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-Unreleased
+0.2 - 2021-11-10
 ----------
 
 Added
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, Peter Macgregor'
 author = 'Peter Macgregor'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.2'
 
 
 # -- General configuration ---------------------------------------------------
