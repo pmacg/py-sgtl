aa4a55c41e255cc36ee4a8672be2bcdf97610cc3
Peter Macgregor <macgregor.pr@gmail.com>
2021-11-10 17:26:20 +0000

    Publish package

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # py-sgtl
-A library of spectral graph theory methods in python.
+SGTL is a python library of spectral graph theory methods. The library is still very new and so
+there are many features and algorithms missing.
+
+You can install the library with `pip install sgtl`.
+
+## Features
+* Lightweight and flexible graph object which can be extended as needed.
+* Graph matrices including the Laplacian and Normalised Laplacian
+* Spectral clustering method
+
+## Documentation
+
+
+## Contributing
+Please feel free to contribute to this project by opening issues, reporting bugs, and submitting
+pull requests.
+
+The library is currently maintained by Peter Macgregor (`peter.macgregor@ed.ac.uk`) and you are
+welcome to get in touch with any questions.
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+DESCRIPTION = 'SGTL - Spectral Graph Theory Library'
+LONG_DESCRIPTION =\
+    "This library provides several methods and algorithms relating to spectral graph theory in python."
+
+# Setting up
+setup(
+    name="sgtl",
+    version=VERSION,
+    author="Peter Macgregor",
+    author_email="<macgregor.pr@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["numpy", "scipy", "sklearn"],
+
+    keywords=['python', 'spectral', 'graph', 'algorithms', 'clustering', 'cheeger'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        'Operating System :: POSIX :: Linux'
+    ]
+)
